3bd3a9cf32ee4bac3b461ba6c29bfdb43380db9b
Tyler Keeling <Tyler.Keeling@eideticom.com>
2020-07-10 11:33:16 -0600

    run_perf_test: Fix skewed values caused by stderr
    Previously, any stderr would be written to result_comp and result_decomp.
    Since throughput is calculated with `awk ... result_comp`, stderr messages
    can vastly skew the reported compthroughput value. This problem is also
    theoretically possible for decompthroughput, but it was not observed.
    
    We fix this by only writing stdout to result_comp and result_decomp, NOT
    stderr. Instead, stderr is written to result_comp_stderr/
    result_decomp_stderr, which are deleted when this script is invoked.
    
    Signed-off-by: TyKeeling <tylerwkeeling@gmail.com>
    Signed-off-by: David Qian <david.qian@intel.com>

--- test/performance_tests/run_perf_test.sh
+++ test/performance_tests/run_perf_test.sh
@@ -36,6 +36,10 @@
 
 set -e
 echo "***QZ_ROOT run_perf_test.sh start"
+
+rm -f result_comp_stderr
+rm -f result_decomp_stderr
+
 CURRENT_PATH=`dirname $(readlink -f "$0")`
 
 #check whether test exists
@@ -85,7 +89,7 @@ thread=4
 echo > result_comp
 for((numProc_comp = 0; numProc_comp < $process; numProc_comp ++))
 do
-    $QZ_ROOT/test/test -m 4 -l 1000 -t $thread -D comp >> result_comp 2>&1  &
+    $QZ_ROOT/test/test -m 4 -l 1000 -t $thread -D comp >> result_comp 2>> result_comp_stderr &
 done
 wait
 compthroughput=`awk '{sum+=$8} END{print sum}' result_comp`
@@ -94,7 +98,7 @@ echo "compthroughput=$compthroughput Gbps"
 echo > result_decomp
 for((numProc_decomp = 0; numProc_decomp < $process; numProc_decomp ++))
 do
-    $QZ_ROOT/test/test -m 4 -l 1000 -t $thread -D decomp >> result_decomp 2>&1  &
+    $QZ_ROOT/test/test -m 4 -l 1000 -t $thread -D decomp >> result_decomp 2>> result_decomp_stderr &
 done
 wait
 decompthroughput=`awk '{sum+=$8} END{print sum}' result_decomp`
